14043b3daa36a6c917d1f47d8a89ab54e13a450d
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-01-01 10:29:35 +0900

    feat: 初期設定

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// GraphQLのスキーマ定義
+const typeDefs = gql`
+  type Query {
+    info: String!
+  }
+`;
+
+// リゾルバ関数
+const resolvers = {
+  Query: {
+    info: () => "HackerNewsクローン",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`${url}でサーバーを起動中`));

17c23a7cb4e3fecc9da757b17c7415fd5d2dffcb
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-01-01 14:38:53 +0900

    feat: PrismaStudioの導入

--- src/server.js
+++ src/server.js
@@ -2,34 +2,29 @@ const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-// HackerNewsの一つ一つの情報
-let links = [
-  {
-    id: "link-0",
-    description: "GraphQLチュートリアルをUdemyで学ぶ",
-    url: "www.udemy-graphql-tutorial.com",
-  },
-];
+const { PrismaClient } = require("@prisma/client");
+const { url } = require("inspector");
+
+const prisma = new PrismaClient();
 
 // リゾルバ関数
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
 };
@@ -37,6 +32,10 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  // resolverの中でどこでも使用できる
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバーを起動中`));
